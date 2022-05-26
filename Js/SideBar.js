da0a7bc6a2ad7dde2c0f95711656d8441ec29db9
andres Londoño <andresmedellin215@gmail.com>
2022-05-26 00:46:46 -0500

    se hace el side bar pero le falta mejorarlo

---
+++ Js/SideBar.js
@@ -0,0 +1,11 @@
+// sidebar toggle ESTO ES DE OTRO SIDE BAR
+
+/* 
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('sidebar').classList.toggle('active');
+});
+
+*/
